cc738362e24c8205bc81babeb8e5a9f0629bf100
Roman Vyshnevskyi <romanvisssh@gmail.com>
2025-12-02 20:15:06 +0100

    db.json

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,6 @@ import { Platform } from 'react-native';
 
 const emulatorAndroid = 'http://10.0.2.2:8083';
 const emulatorIOS = 'http://localhost:8083';
-// !!! заміни IP на IP свого ПК у локальній мережі
 const lanPC = 'http://192.168.0.100:8083';
 
 export const API_BASE_URL = Platform.select({
