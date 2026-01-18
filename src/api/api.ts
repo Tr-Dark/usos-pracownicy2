947c4d98d2cdcdcf3d2eaf097263ce215724cf62
Roman Vyshnevskyi <romanvisssh@gmail.com>
2026-01-18 17:21:54 +0100

    json server dla mobile

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,18 +1,22 @@
 // src/api/api.ts
-import axios from 'axios';
-import { Platform } from 'react-native';
+import axios from "axios";
+import { Platform } from "react-native";
+import * as Device from "expo-device";
 
-const emulatorAndroid = 'http://10.0.2.2:8083';
-const emulatorIOS = 'http://localhost:8083';
-const lanPC = 'http://192.168.0.100:8083';
+const PORT = 8083;
 
-export const API_BASE_URL = Platform.select({
-  ios: emulatorIOS,
-  android: emulatorAndroid,
-  default: lanPC,
-});
+const LAN = `http://192.168.1.12:${PORT}`;
+
+const ANDROID_EMULATOR = `http://10.0.2.2:${PORT}`;
+const IOS_SIMULATOR = `http://localhost:${PORT}`;
+
+export const API_BASE_URL =
+  Platform.OS === "android"
+    ? (Device.isDevice ? LAN : ANDROID_EMULATOR)
+    : IOS_SIMULATOR; 
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
   timeout: 8000,
 });
+
